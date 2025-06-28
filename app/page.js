00c036f355e96844dda42cead7b16527f908b77a
sarrthak008 <sarthaknavale08@gmail.com>
2025-06-28 09:00:29 +0300

    add keycard ui

--- app/page.js
+++ app/page.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import HeroSection from './components/HeroSection'
 
 function page() {
   return (
-    <div>page</div>
+    <div>
+      <HeroSection/>
+    </div>
   )
 }
 
