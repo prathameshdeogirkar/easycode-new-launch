e6dd2a92e8297444bd1bfd020042c57ee2b2d8ae
sarrthak008 <sarthaknavale08@gmail.com>
2025-07-01 11:14:21 +0300

    add a new animation anf change the website title

--- app/components/KeyCard.jsx
+++ app/components/KeyCard.jsx
@@ -1,57 +1,69 @@
-"use client"
-import React, { useRef, useState } from 'react'
-import { gsap } from 'gsap'
+"use client";
+import React, { useRef, useState } from "react";
+import { gsap } from "gsap";
 
+const KeyCard = ({ index, point }) => {
+  const boxRef = useRef(null);
+  const [isCardOpen, setIsCardOpen] = useState(false);
 
-const KeyCard = ({index,point}) => {
-
-   const boxref = useRef(null)
-   const [isCardOpen,setIsCardOpen] = useState(false)
-
-
-   const handelOpning = () => {
-    gsap.to(boxref.current, {
-      minHeight: "300px",
-      duration: 0.5,
+  const handleOpening = () => {
+    gsap.to(boxRef.current, {
+      height: "auto",
+      opacity: 1,
+      duration: 0.8,
       ease: "power2.out",
-      color: "white",
       display: "block",
     });
   };
 
-
-   const handelClosing = () => {
-     gsap.to(boxref.current, {
-      height: "0px",
-        duration: 0.5,
-        color: "black",
-        display: "none",
-        ease: "power2.out",
+  const handleClosing = () => {
+    gsap.to(boxRef.current, {
+      height: 0,
+      opacity: 0,
+      duration: 0.6,
+      ease: "power2.inOut",
+      onComplete: () => {
+        boxRef.current.style.display = "none";
+      },
     });
-   }
+  };
 
-   const toggleCard = () => {
-    if(!isCardOpen){
-      handelOpning()
-      setIsCardOpen(true)
-    }else{
-      handelClosing()
-      setIsCardOpen(false)
+  const toggleCard = () => {
+    if (!isCardOpen) {
+      handleOpening();
+      setIsCardOpen(true);
+    } else {
+      handleClosing();
+      setIsCardOpen(false);
     }
-   }
-
+  };
 
   return (
     <>
-      <div className='min-h-[90px] border-t-[1px] border-t-white mb-10 mt-2 px-5 text-3xl flex items-center justify-between'>
-        <span className='text-xl sm:text-4xl'><span>{index+1}. </span>{point?.point_name}</span>
-        <span className='text-blue-500 text-2xl cursor-pointer' onClick={()=>toggleCard()} >
-          <span className='text-xl sm:text-2xl'>more</span>
-          <i className={`${isCardOpen ? "ri-arrow-up-line" : "ri-arrow-down-line"}`}></i>
+      <div className="min-h-[90px] border-t-[1px] border-t-white mb-10 mt-2 px-5 text-3xl flex items-center justify-between">
+        <span className="text-xl sm:text-4xl">
+          <span>{index + 1}. </span>
+          {point?.point_name}
+        </span>
+        <span
+          className="text-blue-500 text-2xl cursor-pointer"
+          onClick={toggleCard}
+        >
+          <span className="text-xl sm:text-2xl">more </span>
+          <i
+            className={`${
+              isCardOpen ? "ri-arrow-up-line" : "ri-arrow-down-line"
+            }`}
+          ></i>
         </span>
       </div>
-      <div className=' key-points-box hidden' ref={boxref}>
-        {point?.point_description}
+
+      <div
+        ref={boxRef}
+        className="key-points-box text-xl overflow-hidden opacity-0 hidden px-5"
+        style={{ height: 0 }}
+      >
+        <span className="inline-block mb-10">{point?.point_description}</span>
       </div>
     </>
   );
