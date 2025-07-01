f766cff8420b923299cdfecbf00840196d6acd0e
sarrthak008 <sarthaknavale08@gmail.com>
2025-07-01 10:39:19 +0300

    add a loading screen

--- app/page.js
+++ app/page.js
@@ -1,38 +1,43 @@
 "use client"
-import React, { useEffect, useState } from 'react'
+import React, { Suspense, useEffect, useState } from 'react'
 import HeroSection from './components/HeroSection'
 import axios from 'axios'
 import Navbar from './components/Navbar'
+import LoadingComp from './components/LoadingComp'
 
 function page() {
 
-   const [latestData, setLaunchData] = useState()
+  const [latestData, setLaunchData] = useState()
+  const [isApiFetch, setIsAPiFetch] = useState(false)
 
-   const loadLaunchData = async () => {
-      try {
-         let result = await axios("/api/newlaunch");
-         
-         if(result){
-            setLaunchData(result.data?.data?.reverse()[0])
-         }
-        
-      } catch (error) {
-        
+  const loadLaunchData = async () => {
+    try {
+      let result = await axios("/api/newlaunch");
+
+      if (result) {
+        setLaunchData(result.data?.data?.reverse()[0])
+        setIsAPiFetch(true)
       }
-   }
-
-   console.log(latestData)
-
-
-   useEffect(()=>{
-     loadLaunchData()
-   },[])
+    } catch (error) {
+      console.error("error:", error.message)
+    }
+  }
+  console.log(latestData)
+  useEffect(() => {
+    loadLaunchData()
+  }, [])
 
   return (
-    <div>
-      <Navbar/>
-      <HeroSection data={latestData}/>
-    </div>
+    <>
+      {
+        !isApiFetch ? <LoadingComp /> :
+
+          <>
+            <Navbar />
+            <HeroSection data={latestData} />
+          </>
+      }
+    </>
   )
 }
 
